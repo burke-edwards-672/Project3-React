eef4a4be7fc631ef4e8ee2faa28e0d8122affa7c
Burke Edwards <burkeedwardspersonal@gmail.com>
2026-04-02 16:57:53 -0500

    added functionality for edit page

--- client/src/components/edit-card.jsx
+++ client/src/components/edit-card.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
 
-export default function EditCard({ frontText, backText }) {
-    const [front, setFront] = useState("front");
+export default function EditCard({ index, front, back, id, edit, remove}) {
+    const [isModifying, toggleModify] = useState(false);
 
+    function saveChanges(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const formObject = Object.fromEntries(formData.entries());
+        formObject.key = id;
+
+        console.log(formObject);
+        edit(formObject);
+        toggleModify(false);
+    }
+
+    if (isModifying) return (
+        <form className="edit-card" onSubmit={saveChanges}>
+            <textarea name="front" defaultValue={front} />
+            <textarea name="back" defaultValue={back} />
+            <p>{index}</p>
+            <button name="Save" type="submit">Save</button>
+            <button name="Delete" type="button" onClick={remove}>Delete</button>
+        </form>
+    );
     return (
-        <div className="edit-card">
-            <textarea/>
-            <textarea/>
-            <button name="Delete" type="button" >Delete</button>
-        </div>
+            <div className="edit-card">
+                <p>{front}</p>
+                <p>{back}</p>
+                <p>{index}</p>
+                <button name="Edit" type="button" onClick={() => {toggleModify(true);}}>Edit</button>
+                <button name="Delete" type="button" onClick={remove}>Delete</button>
+            </div>
     )
 }
--- client/src/pages/edit.jsx
+++ client/src/pages/edit.jsx
@@ -8,6 +8,7 @@ import EditCard from "../components/edit-card";
 
 export default function Edit(recents) {
     const [cards, setCards] = useState([]);
+    const [newId, setNewId] = useState(0);
 
     //When I learned that [] makes this only run on the initial render, I thought that was just the initial render of the website.
     //Nope! It goes every time the whole thing pops up. That's incredibly useful. I can make this code so much cleaner knowing that.
@@ -16,17 +17,57 @@ export default function Edit(recents) {
             const metadata = await api.getMetadata();
             const deck = await api.getDeck(metadata.recentIds[0])
             setCards(deck.questions);
+            setCards(cards => keyCards(cards))
+            setNewId(deck.questions.length)
         })();
     }, [])
 
-    function makeCards() {
-        return cards.map((card) => {
+    function appendCard() {
+        const key = `Q ${newId}`;
+        const newCard = {
+            front: "Front",
+            back: "Back",
+            key: key
+        };
+        setCards([...cards, newCard]);
+        setNewId(id => id + 1);
+    }
+
+    function modifyCard(newCard) {
+        console.log("editing!!!");
+        console.log(newCard);
+        setCards(cards.map(c => {
+            if (c.key === newCard.key) {
+                return newCard;
+            } else {
+                return c;
+            }
+        }))
+    }
+
+    function removeCard(key) {
+        setCards(cards.filter(c => key !== c.key));
+        //setCards(cards => keyCards(cards));
+    }
+
+    function keyCards(questions) {
+        return questions.map((question, index) => {
+            return {...question, key: `Q ${index.toString()}`};
+        })
+    }
+
+    function mapCards() {
+        return cards.map((card, index) => {
             if (card) {
                 return (
-                    <>
-                        <p>{card.front}</p>
-                        <p>{card.back}</p>
-                    </>
+                    <EditCard 
+                    key={card.key}
+                    index={index}
+                    front={card.front}
+                    back={card.back}
+                    id={card.key}
+                    edit={modifyCard}
+                    remove={() => removeCard(card.key)}/>
                 )
             }
         })
@@ -35,25 +76,14 @@ export default function Edit(recents) {
     function post(formData) {
         alert(formData.get("test-box"));
     }
-
-    function deleteCard(formData) {
-        alert(`YOU DELETED`);
-    }
     return (
         <>
             <Header />
             <main>
-                <form action={post}>
 
-                    <EditCard />
-                    <EditCard />
-                    <EditCard />
-
-                    <button type="submit">Submit</button>
-                </form>
             </main>
-            <button><img src="/images/icons/plus.jpg" /></button>
-            {makeCards()}
+            <button onClick={appendCard}><img src="/images/icons/plus.jpg" /></button>
+            {mapCards()}
             <Footer />
         </>
     )
